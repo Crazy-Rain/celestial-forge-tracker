5c6823133f54fa5b57a9d46c56b139fe381724dc
Crazy-Rain <magejames@hotmail.com>
2026-01-24 05:26:45 +1000

    Update index.js

--- index.js
+++ index.js
@@ -676,7 +676,7 @@ function generateSimTrackerJSON() {
     
     const jsonData = {
         characters: [{
-            characterName: context.character.name",
+            characterName: context.character.name,
             currentDateTime: new Date().toLocaleString(),
             stats: {
                 total_cp: totalCP,
